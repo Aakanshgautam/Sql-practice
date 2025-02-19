e372cfdb0d03a7fc96f74757ce0723b8c5d500fa
Aakanshgautam <aakanshgautam0088@gmail.com>
2025-02-20 00:06:08 +0530

    Solution Q2

--- SQL Ques-2.sql
+++ SQL Ques-2.sql
@@ -12,10 +12,11 @@ Boston, MA  | 365           | 25696.35
 Chicago, IL | 275           | 21997.25``` */
 
 
-select w.location,i.quantity as toatal_quantity, (i.quantity * p.base_price) as total_value from warehouses as w
-inner join inventory  as i
+select w.location,i.quantity as toatal_quantity, (i.quantity * p.base_price) as total_value   -- columns we need
+from warehouses as w
+inner join inventory  as i                         -- we need values that match in warehoues table and product table                                        
 on w.warehouse_id= i.warehouse_id
-inner join products as p
+inner join products as p                               -- join product table with inventory table
 on i.product_id= p.product_id 
-where p.category = 'Electronics'  and  (i.quantity * p.base_price) >20000
+where p.category = 'Electronics'  and  (i.quantity * p.base_price) >20000      -- filter based on category and price 
 order by  total_value desc;
